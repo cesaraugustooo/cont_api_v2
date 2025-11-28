12580b1642bbaf065ef32848663dcbfb79065121
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-11-28 14:48:18 -0300

    feature: added field(status) of Autorizados entity

--- database/migrations/2025_11_28_134703_create_autorizados_table.php
+++ database/migrations/2025_11_28_134703_create_autorizados_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('titulo');
             $table->longText('conteudo');
+            $table->enum('status',['pendente','confirmado']);
             $table->timestamps();
         });
     }

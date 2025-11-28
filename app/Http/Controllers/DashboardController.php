592ef908b248fca5445bca10428a7360dc2d11bb
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-11-28 10:50:56 -0300

    fixed: added data query paramter

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,7 +8,15 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function contagemDashboard(){
+    public function contagemDashboard(Request $request){
+        if($request->query('data')){
+            $data = Categoria::with(['turmas.contagen'=>function($query) use($request){
+                $query->where('data_contagem',$request->query('data'));
+            }])->get();
+                    
+            return DashboardResource::collection($data);
+        }
+
         $data = Categoria::with(['turmas.contagen'=>function($query){
             $query->where('data_contagem',date('Y-m-d'));
         }])->get();
